0bc347352c94f9465c04f9c066114f7de84d3749
Sameer-77 <123528477+Sameer-77@users.noreply.github.com>
2024-11-27 10:16:42 +0530

    Add files via upload

---
+++ BINARY TREES/TRAVERSALS/InOrder.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+TreeNode *buildTree()
+{
+    int data;
+    cout << "Enter the root value : ";
+    cin >> data;
+    if (data == -1)
+    {
+        return nullptr;
+    }
+
+    TreeNode *root = new TreeNode(data);
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        TreeNode *curr = q.front();
+        q.pop();
+        cout << "Enter the Left Child of " << curr->data << " : ";
+        cin >> data;
+        if (data != -1)
+        {
+            curr->left = new TreeNode(data);
+            q.push(curr->left);
+        }
+        cout << "Enter the Right Child of " << curr->data << " : ";
+        cin >> data;
+        if (data != -1)
+        {
+            curr->right = new TreeNode(data);
+            q.push(curr->right);
+        }
+    }
+    return root;
+}
+
+void InOrder(TreeNode *root)
+{
+    if (root == NULL)
+        return;
+    InOrder(root->left);
+    cout << root->data << " ";
+    InOrder(root->right);
+}
+
+int main()
+{
+    TreeNode *root = buildTree();
+    InOrder(root);
+}
---
+++ BINARY TREES/TRAVERSALS/PreOrder.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TreeNode
+{
+public:
+    int data;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+TreeNode *buildTree()
+{
+    int data;
+    cout << "Enter the root value : ";
+    cin >> data;
+    if (data == -1)
+    {
+        return nullptr;
+    }
+
+    TreeNode *root = new TreeNode(data);
+    queue<TreeNode *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        TreeNode *curr = q.front();
+        q.pop();
+        cout << "Enter the Left Child of " << curr->data << " : ";
+        cin >> data;
+        if (data != -1)
+        {
+            curr->left = new TreeNode(data);
+            q.push(curr->left);
+        }
+        cout << "Enter the Right Child of " << curr->data << " : ";
+        cin >> data;
+        if (data != -1)
+        {
+            curr->right = new TreeNode(data);
+            q.push(curr->right);
+        }
+    }
+    return root;
+}
+
+void PreOrder(TreeNode *root)
+{
+    if (root == NULL)
+    {
+        return;
+    }
+    cout << root->data << " ";
+    PreOrder(root->left);
+    PreOrder(root->right);
+}
+
+int main()
+{
+    TreeNode *root = buildTree();
+    PreOrder(root);
+}
